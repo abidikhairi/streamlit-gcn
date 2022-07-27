80a0ede29d0ba7659d7905fdf5eaf8a2c25ff2c1
abidikhairi <abidikhairi.1337@gmail.com>
2022-07-27 21:01:47 +0200

    draw model architecture

--- pages/architectures.py
+++ pages/architectures.py
@@ -1,4 +1,15 @@
 import streamlit as st
 
 
-st.title('Architectures')
+st.title('GCN Architectures used in this tutorial')
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    col1.header('2 Layer GCN')
+    col1.image('figures/2layer_gcn.png')
+
+with col2:
+    col2.header('Random GCN')
+    col2.image('figures/random_gcn.png')
